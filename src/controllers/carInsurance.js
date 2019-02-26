6abb5ed76313702cbdd4878d4f8060375f9f3088
Rut <martinezrut619@gmail.com>
2019-02-26 20:17:32 -0300

    update in test and updatePrice

--- src/controllers/carInsurance.js
+++ src/controllers/carInsurance.js
@@ -4,6 +4,7 @@ class CarInsurance {
       this.products = products;
       this.priceMax = 50;
       this.priceMin = 0;
+      this.amountDaysDecrease = 1;
     }
 
     setTopPrice(price) {
@@ -14,30 +15,22 @@ class CarInsurance {
       return (price <= this.priceMin) ? this.priceMin : price;
     }
 
-    isDateExpiry(sellIn) {
+    isSellbyDate(sellIn) {
       return sellIn < 0
     }
 
-    decreasePrice(sellIn, beforeDateExpiry, afterDateExpiry) {
-      return this.isDateExpiry(sellIn) ? afterDateExpiry : beforeDateExpiry;
+    getPriceDependingDate(sellIn, beforeSellbyDate, afterSellbyDate) {
+      return this.isSellbyDate(sellIn) ? afterSellbyDate : beforeSellbyDate;
     }
 
-    increasePrice(sellIn, beforeDateExpiry, afterDateExpiry) {
-      return this.isDateExpiry(sellIn) ? afterDateExpiry : beforeDateExpiry;
-    }
-
-    increasePriceDependingDay(sellIn) {
-      if (sellIn < 10) {
-        return this.increasePrice(sellIn, 2, 0)
-      }
+    getIncreasePriceDependingDay(sellIn) {
       if (sellIn < 5) {
-        return this.increasePrice(sellIn, 3, 0)
+        return this.getPriceDependingDate(sellIn, 3, 0)
       }
-      return this.increasePrice(sellIn, 1, 2);
-    }
-
-    decreaseDate(sellIn) {
-      return 1;
+      if (sellIn < 10) {
+        return this.getPriceDependingDate(sellIn, 2, 0)
+      }
+      return this.getPriceDependingDate(sellIn, 1, 2);
     }
 
     updatePrice() {
@@ -47,34 +40,32 @@ class CarInsurance {
           case 'Mega Coverage':
             break;
           case 'Special Full Coverage':
-            this.products[i].sellIn -= this.decreaseDate(this.products[i].sellIn);
-            this.products[i].price  += this.increasePriceDependingDay(this.products[i].sellIn);
+            this.products[i].sellIn -= this.amountDaysDecrease;
+            this.products[i].price  += this.getIncreasePriceDependingDay(this.products[i].sellIn);
             this.products[i].price = this.setTopPrice(this.products[i].price);
             this.products[i].price = this.setBottomPrice(this.products[i].price);
         
             break;
           case 'Full Coverage':
-            this.products[i].sellIn -= this.decreaseDate(this.products[i].sellIn);
-            this.products[i].price  += this.increasePrice(this.products[i].sellIn, 1, 2);
+            this.products[i].sellIn -= this.amountDaysDecrease;
+            this.products[i].price  += this.getIncreasePriceDependingDay(this.products[i].sellIn);
             this.products[i].price = this.setTopPrice(this.products[i].price);
             this.products[i].price = this.setBottomPrice(this.products[i].price);
         
             break
           case 'Super Sale':
-            this.products[i].sellIn -= this.decreaseDate(this.products[i].sellIn);
-            this.products[i].price  -= this.decreasePrice(this.products[i].sellIn, 2, 2);
+            this.products[i].sellIn -= this.amountDaysDecrease;
+            this.products[i].price  -= this.getPriceDependingDate(this.products[i].sellIn, 2, 2);
             this.products[i].price = this.setTopPrice(this.products[i].price);
             this.products[i].price = this.setBottomPrice(this.products[i].price);
             break
           
           default:
-            this.products[i].sellIn -= this.decreaseDate(this.products[i].sellIn);
-            this.products[i].price  -= this.decreasePrice(this.products[i].sellIn, 1, 2);
+            this.products[i].sellIn -= this.amountDaysDecrease;
+            this.products[i].price  -= this.getPriceDependingDate(this.products[i].sellIn, 1, 2);
             this.products[i].price = this.setTopPrice(this.products[i].price);
             this.products[i].price = this.setBottomPrice(this.products[i].price);
         }
-
-        
       }
   
       return this.products;
--- test/carInsuranceTestSpec.js
+++ test/carInsuranceTestSpec.js
@@ -7,9 +7,9 @@ const models = require('../src/models/Product');
 const Product = models.Product;
 
 describe("CarInsurance", () => {
+  const coTest = new CarInsurance([]);
 
   it("should return the price correct  when exceeds the allowed range", () => {
-    const coTest = new CarInsurance([]);
 
     const priceOverflow = coTest.setTopPrice(80);
     expect(priceOverflow).equal(coTest.priceMax);
@@ -24,17 +24,102 @@ describe("CarInsurance", () => {
     expect(priceNormalBottom).equal(20);
   });
 
-  it("should verify date of Expiry", () => {
-    const coTest = new CarInsurance([]);
+  it("should verify sell by date", () => {
 
-    const isAfterDateExpiry = coTest.isDateExpiry(-1);
-    expect(isAfterDateExpiry).equal(true);
+    const isAfterSellbyDate = coTest.isSellbyDate(-1);
+    expect(isAfterSellbyDate).equal(true);
 
-    const isBeforeDateExpiry = coTest.isDateExpiry(1);
-    expect(isBeforeDateExpiry).equal(false);
+    const isBeforeSellbyDate = coTest.isSellbyDate(1);
+    expect(isBeforeSellbyDate).equal(false);
 
-    const isDateExpiryNeutral = coTest.isDateExpiry(0);
-    expect(isDateExpiryNeutral).equal(true);
+    const isSellbyDateNeutral = coTest.isSellbyDate(0);
+    expect(isSellbyDateNeutral).equal(false);
   });
 
+  describe("the price depending the sell by date", () => {
+
+    it("should return price specified afterSellbyDate", () => {
+      const sellIn = -1;
+      const beforeSellbyDate = 1;
+      const afterSellbyDate = 2;
+      const getPriceDependingDate = coTest.getPriceDependingDate(sellIn, beforeSellbyDate, afterSellbyDate);
+      expect(getPriceDependingDate).equal(afterSellbyDate);
+    });
+
+    it("should return  amount specified beforeSellbyDate", () => {
+      const sellIn = 5;
+      const beforeSellbyDate = 1;
+      const afterSellbyDate = 2;
+      const getPriceDependingDate = coTest.getPriceDependingDate(sellIn, beforeSellbyDate, afterSellbyDate);
+      expect(getPriceDependingDate).equal(beforeSellbyDate);
+    });
+  })
+
+  describe("the Price depending the quantity days sellIn", () => {
+
+    it("should return price specified for the quantity 9 days sellIn", () => {
+      const sellIn = 9;
+
+      const getPriceDependingDate = coTest.getIncreasePriceDependingDay(sellIn);
+      expect(getPriceDependingDate).equal(2);
+    });
+
+    it("should return price specified for the quantity 4 days sellIn", () => {
+      const sellIn = 4;
+ 
+      const getPriceDependingDate = coTest.getIncreasePriceDependingDay(sellIn);
+      expect(getPriceDependingDate).equal(3);
+    });
+
+    it("should return price specified for the quantity 20 days sellIn", () => {
+      const sellIn = 20;
+ 
+      const getPriceDependingDate = coTest.getIncreasePriceDependingDay(sellIn);
+      expect(getPriceDependingDate).equal(1);
+    });
+
+  })
+  
+  describe("update price", () => {
+
+    it("should return price degrades twice as fast, once the sell by date has passed", () => {
+      
+      const productsAtDayZero = [
+        new Product('Medium Coverage', 0, 10),
+        new Product('Super Sale', 0, 6),
+      ];
+
+      const productMediumCoverage = new CarInsurance(productsAtDayZero)
+      const updateProduct = productMediumCoverage.updatePrice();
+      expect(updateProduct[0].price).equal(8);
+      expect(updateProduct[1].price).equal(4);
+    })
+  })
+
+  it("should verify the price of a product is never negative", () => {
+    
+    const productsAtDayZero = [
+      new Product('Medium Coverage', 10, -2)
+    ];
+
+    const productMediumCoverage = new CarInsurance(productsAtDayZero)
+    const updateProduct = productMediumCoverage.updatePrice();
+
+    expect(updateProduct[0].price).equal(0);
+  })
+
+
+  it("should verify the product 'Full Coverage' actually increases in price the older it gets and the price of a product is never more than 50.", () => {
+    
+    const productsAtDayZero = [
+      new Product('Full Coverage', 2, 0),
+      new Product('Full Coverage', 10, 50)
+    ];
+
+    const productMediumCoverage = new CarInsurance(productsAtDayZero)
+    const updateProduct = productMediumCoverage.updatePrice();
+
+    expect(updateProduct[0].price).equal(1);
+    expect(updateProduct[1].price).equal(50);
+  })
 });
